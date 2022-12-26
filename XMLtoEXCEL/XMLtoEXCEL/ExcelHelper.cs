0a46c6fed71b1dca9f8c19066e2a85e4802d1a36
roderichy27 <121464613+roderichy27@users.noreply.github.com>
2022-12-27 01:12:17 +0200

    Add files via upload

---
+++ XMLtoEXCEL/XMLtoEXCEL/ExcelHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace XMLtoEXCEL
+{
+
+    class ExcelHelper : IDisposable
+    {
+        private Excel.Application _excel;
+        private Excel.Workbook _workbook;
+        private string _filePath;
+
+        public ExcelHelper()
+        {
+            _excel = new Excel.Application();
+        }
+
+        internal bool Open(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    _workbook = _excel.Workbooks.Open(filePath);
+                }
+                else
+                {
+                    _workbook = _excel.Workbooks.Add();
+                    _filePath = filePath;
+                }
+
+                return true;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return false;
+        }
+
+        internal void Save()
+        {
+            if (!string.IsNullOrEmpty(_filePath))
+            {
+                _workbook.SaveAs(_filePath);
+                _filePath = null;
+            }
+            else
+            {
+                _workbook.Save();
+            }
+        }
+
+        internal bool Set(string column, int row, object data)
+        {
+            try
+            {
+                // var val = ((Excel.Worksheet)_excel.ActiveSheet).Cells[row, column].Value2;
+
+                ((Excel.Worksheet)_excel.ActiveSheet).Cells[row, column] = data;
+                return true;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return false;
+        }
+
+        internal object Get(string column, int row)
+        {
+            try
+            {
+                return ((Excel.Worksheet)_excel.ActiveSheet).Cells[row, column].Value2;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return null;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _workbook.Close();
+                _excel.Quit();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
---
+++ XMLtoEXCEL/XMLtoEXCEL/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLtoEXCEL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load("1.xml");
+                XmlElement xmlRoot = xmlDoc.DocumentElement;
+                foreach (XmlNode xnode in xmlRoot)
+                {
+                    string ts = xnode.Attributes[0].Value;
+                    Console.WriteLine(ts);
+                }
+
+                using (ExcelHelper helper = new ExcelHelper())
+                {
+                    if (helper.Open(filePath: Path.Combine(Environment.CurrentDirectory, "TestLT.xlsx")))
+                    {
+                        helper.Set(column: "A", row: 1, data: "Crycry");
+                        var val = helper.Get(column: "A", row: 6);
+                        helper.Set(column: "B", row: 1, data: DateTime.Now);
+                        helper.Set(column: "A", row: 3, data: "Crycry");
+
+                        helper.Save();
+                    }
+                }
+
+                Console.Read();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
